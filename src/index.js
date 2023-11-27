5d492ee58e5f6003ba5813a9e4200a3d84b138ca
sh0nnie4 <139727327+sh0nnie4@users.noreply.github.com>
2023-11-27 18:06:16 -0500

    Converted code from CodeSandbox to VS Code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+import WeatherSearch from "./WeatherSearch";
+
+import "./App.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
   </React.StrictMode>
 );
 
